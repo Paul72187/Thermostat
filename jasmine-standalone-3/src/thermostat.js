34b85c376e83956f12fdd9b7767d6dcdc04b9511
Sophie Bonneval <maldorana@gmail.com>
2021-09-29 16:40:50 +0100

    Bugs fixed

--- jasmine-standalone-3/src/thermostat.js
+++ jasmine-standalone-3/src/thermostat.js
@@ -1,60 +1,59 @@
+'use strict';
+
 class Thermostat {
-
-    'use strict';
-
-    constructor() {
-        this.DEFAULT_TEMPERATURE = 20;
-        this.MINIMUM_TEMPERATURE = 10;
-        this.temperature = this.DEFAULT_TEMPERATURE;
-        this.MAX_LIMIT_PSM_0N = 25;
-        this.MAX_LIMIT_PSM_OFF = 32;
-        this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
-        this.HIGH_ENERGY_USAGE_LIMIT = 25;
-        this.powerSavingMode = true;
+  constructor() {
+    this.MINIMUM_TEMPERATURE = 10;
+    this.MAXIMUM_TEMPERATURE_PSM_ON = 25;
+    this.MAXIMUM_TEMPERATURE_PSM_OFF = 32;
+    this.powerSavingMode = true;
+    this.DEFAULT_TEMPERATURE = 20;
+    this.temperature = this.DEFAULT_TEMPERATURE;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
+  }
+  getCurrentTemperature() {
+    return this.temperature;
+  }
+  resetTemperature() {
+    this.temperature = this.DEFAULT_TEMPERATURE;
+  }
+  isMinimumTemperature() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+  isMaximumTemperature() {
+    if (this.isPowerSavingModeOn()) {
+      return this.temperature === this.MAXIMUM_TEMPERATURE_PSM_ON;
     }
-    getCurrentTemperature() {
-        return this.temperature;
+    return this.temperature === this.MAXIMUM_TEMPERATURE_PSM_OFF;
+  }
+  isPowerSavingModeOn() {
+    return this.powerSavingMode === true;
+  }
+  switchPowerSavingModeOff() {
+    this.powerSavingMode = false;
+  }
+  switchPowerSavingModeOn() {
+    this.powerSavingMode = true;
+  }
+  up() {
+    if (this.isMaximumTemperature()) {
+      return;
     }
-    isMinimumTemperature() {
-        return this.temperature === this.MINIMUM_TEMPERATURE;
+    this.temperature += 1;
+  }
+  down() {
+    if (this.isMinimumTemperature()) {
+      return;
     }
-    isMaximumTemperature() {
-        if (this.isPowerSavingModeOn() === false) {
-            return this.temperature === this.MAX_LIMIT_PSM_OFF;
-        }
-        return this.temperature === this.MAX_LIMIT_PSM_ON;
+    this.temperature -= 1;
+  }
+  energyUsage() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
     }
-    isPowerSavingModeOn() {
-        return this.powerSavingMode === true;
+    if (this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT) {
+      return 'medium-usage';
     }
-    up() {
-        if (this.isMaximumTemperature()) {
-            return;
-        }
-        this.temperature += 1
-    }
-    down() {
-        if (this.isMinimumTemperature()) {
-            return;
-        }
-        this.temperature -= 1
-    }
-    switchPowerSavingModeOff() {
-        this.powerSavingMode = false;
-    }
-    switchPowerSavingModeOn() {
-        this.powerSavingMode = true;
-    }
-    resetTemperature() {
-        this.temperature = this.DEFAULT_TEMPERATURE;
-    }
-    energyUsage() {
-        if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
-            return 'low-usage';
-        }
-        if (this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT) {
-            return 'medium-usage';
-        }
-        return 'high-usage';
-    }
-}
+    return 'high-usage';
+  }
+};
