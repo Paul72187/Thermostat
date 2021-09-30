cc8385e77ba37a0bbfbd4d06b06e620784b78a9a
Admin <admin@Admins-MacBook-Pro.local>
2021-09-30 16:48:11 +0100

    Finished API

--- jasmine-standalone-3/src/interface.js
+++ jasmine-standalone-3/src/interface.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const updateTemperature = () => {
         document.querySelector('#temperature').innerText = thermostat.temperature;
+        document.querySelector('#temperature').className = thermostat.energyUsage();
     }
     const thermostat = new Thermostat();
     updateTemperature();
@@ -28,5 +29,34 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('#powersaving-off').addEventListener('click', () => {
         thermostat.switchPowerSavingModeOff();
         updateTemperature();
+    })
+
+    //fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+    //.then((response) => {
+    //return response.json()
+    //})
+    //.then((data) => {
+    //document.querySelector('#current-temperature').innerText = data.main.temp;
+    //});
+
+    const displayWeather = (city) => {
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+
+        fetch(url)
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                document.querySelector('#current-temperature').innerText = data.main.temp;
+            })
+    }
+
+    displayWeather('London');
+
+    document.querySelector('#current-city').addEventListener('change', (event) => {
+        event.preventDefault();
+        const city = document.querySelector('#current-city').value;
+
+        displayWeather(city);
     })
 })
